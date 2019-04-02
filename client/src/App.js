35c1ba628b6e958c3ba2ad9b05af71d44c2b1095
Divya Nair <divnairs@gmail.com>
2019-04-02 13:50:51 -0700

    finished part 2 stretch

--- client/src/App.js
+++ client/src/App.js
@@ -13,12 +13,23 @@ export default class App extends Component {
     }
   }
 
+  addToSavedList = movie => {
+    const savedList = this.state.savedList
+    savedList.push(movie)
+    this.setState({ savedList })
+  }
+
   render() {
     return (
       <React.Fragment>
         <SavedList list={this.state.savedList} />
         <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={Movie} />
+        <Route
+          path="/movies/:id"
+          render={props => (
+            <Movie {...props} addToSavedList={this.addToSavedList} />
+          )}
+        />
       </React.Fragment>
     )
   }
