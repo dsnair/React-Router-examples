269d2b62c1dc6a58fe6af800e6cb9ad7a1aa909e
Divya Nair <divnairs@gmail.com>
2019-04-01 20:53:54 -0700

    finish basic nav

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <div className="app">
+      <h1>React Router Mini</h1>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </div>
+  )
+}
+
+export default Navigation
