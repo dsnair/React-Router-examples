eac0fb42c63441cb8e92833efe98d3e930574be2
Divya Nair <divnairs@gmail.com>
2019-04-03 13:01:07 -0700

    display API GET data

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './App.css'
 
 class App extends Component {
+  state = {
+    friends: []
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/friends')
+      .then(response => this.setState({ friends: response.data }))
+      .catch(error => console.error(error))
+  }
+
   render() {
-    return <>hi</>
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>E-mail</th>
+            <th>Age</th>
+          </tr>
+        </tbody>
+        {this.state.friends.map(friend => (
+          <tbody key={friend.id}>
+            <tr>
+              <th>{friend.name}</th>
+              <th>{friend.email}</th>
+              <th>{friend.age}</th>
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    )
   }
 }
 
